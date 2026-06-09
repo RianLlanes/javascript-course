f3b927ac9246b81724e16ad30aee460f587544bc
Rian Llanes <rrcllanes@gmail.com>
2026-06-09 11:13:40 +0800

    Completed Lesson 01 Exercises - Javascript Basics

---
+++ Lesson 01 Exercises - Javascript Basics/1a-j.js
@@ -0,0 +1,29 @@
+// 1a
+alert('Good morning!');
+
+// 1b
+alert('James Bond');
+
+// 1c
+console.log(10 + 5);
+
+// 1d
+console.log(20 - 5);
+
+// 1e
+console.log(2 + 2 - 5);
+
+// 1f
+document.body.innerHTML = 'Good morning!';
+
+// 1g
+document.body.innerHTML = 'James Bond';
+
+// 1h
+console.log(10 + 8 + 20);
+
+// 1i
+console.log(100 - 20 - 50 + 200);
+
+// 1j
+document.body.innerHTML = '';
